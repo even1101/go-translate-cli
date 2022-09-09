f135714e80e3d78bd8a6194484ed8ebee246b264
even.chang <even1101u@gmail.com>
2022-09-09 15:24:39 +0800

    定義cli 參數

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 var sourceLang string
 var targetLang string
 var sourceText string
 
-func main() {}
+func init() {
+	flag.StringVar(&sourceLang, "s", "zh-TW", "輸入的文字語系")
+	flag.StringVar(&targetLang, "t", "en", "翻譯的文字語系")
+	flag.StringVar(&sourceLang, "st", "Text", "翻譯的文字")
+
+}
+
+func main() {
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		fmt.Print("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
